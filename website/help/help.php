0e7e72920268154e7490cb47a31ec315d8aec909
Katharine Berry <katharine@kathar.in>
2010-08-10 10:50:01 +0100

    Use a permanent redirect.

--- website/help/help.php
+++ website/help/help.php
@@ -4,6 +4,7 @@ $language = $_GET['language'];
 $channel = $_GET['channel'];
 $version = $_GET['version'];
 $topic = str_replace('/', '', $_GET['topic']);
+header("HTTP/1.1 301 Moved Permanently");
 if(!file_exists("{$topic}.html"))
 {
     header("Location: http://viewer-help.secondlife.com/{$language}/Second%20Life%20Release/{$version}/{$topic}");
