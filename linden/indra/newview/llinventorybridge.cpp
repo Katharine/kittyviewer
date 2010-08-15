8e2009b6e9e7814b61d923f0553fba133d56e289
Katharine Berry <katharine@kathar.in>
2010-08-15 14:39:07 +0100

    More centralised .wl checks.

--- linden/indra/newview/llinventorybridge.cpp
+++ linden/indra/newview/llinventorybridge.cpp
@@ -3645,14 +3645,13 @@ void LLNotecardBridge::openItem()
 	LLViewerInventoryItem* item = getItem();
 	if (item)
 	{
-		bool is_windlight = (getName().length() > 2 && getName().compare(getName().length() - 3, 3, ".wl") == 0);
-		if(!is_windlight)
+		if(isSkySetting())
 		{
-			LLInvFVBridgeAction::doAction(item->getType(),mUUID,getInventoryModel());
+			LLWLParamManager::instance()->loadPresetNotecard(item->getName(), item->getAssetUUID(), mUUID);
 		}
 		else
 		{
-			LLWLParamManager::instance()->loadPresetNotecard(item->getName(), item->getAssetUUID(), mUUID);
+			LLInvFVBridgeAction::doAction(item->getType(),mUUID,getInventoryModel());
 		}
 	}
 }
@@ -3667,16 +3666,18 @@ void LLNotecardBridge::buildContextMenu(LLMenuGL& menu, U32 flags)
 	}
 	else
 	{
-		bool is_windlight = (getName().length() > 2 && getName().compare(getName().length() - 3, 3, ".wl") == 0);
 		items.push_back(std::string("Share"));
 		if (!canShare())
 		{
 			disabled_items.push_back(std::string("Share"));
 		}
 		
-		if(is_windlight)
+		if(isWindLight())
 		{
-			items.push_back(std::string("Use WindLight Settings"));
+			if(isSkySetting())
+			{
+				items.push_back(std::string("Use WindLight Settings"));
+			}
 			items.push_back(std::string("Edit WindLight Settings"));
 		}
 		else
@@ -3710,8 +3711,7 @@ void LLNotecardBridge::performAction(LLInventoryModel* model, std::string action
 
 LLUIImagePtr LLNotecardBridge::getIcon() const
 {
-	bool is_windlight = (getName().length() > 2 && getName().compare(getName().length() - 3, 3, ".wl") == 0);
-	if(is_windlight)
+	if(isSkySetting())
 	{
 		return LLUI::getUIImage("Inv_WindLight");
 	}
@@ -3719,6 +3719,21 @@ LLUIImagePtr LLNotecardBridge::getIcon() const
 	{
 		return LLInventoryIcon::getIcon(LLAssetType::AT_NOTECARD);
 	}
+}
+
+bool LLNotecardBridge::isSkySetting() const
+{
+	return (getName().length() > 2 && getName().compare(getName().length() - 3, 3, ".wl") == 0);
+}
+
+bool LLNotecardBridge::isWaterSetting() const
+{
+	return (getName().length() > 2 && getName().compare(getName().length() - 3, 3, ".ww") == 0);
+}
+
+bool LLNotecardBridge::isWindLight() const
+{
+	return (isSkySetting() || isWaterSetting());
 }
 
 // +=================================================+
