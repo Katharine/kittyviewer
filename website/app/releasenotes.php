89a348fa810965cba75c2ad13989464628167bc2
Katharine Berry <katharine@kathar.in>
2010-08-17 15:39:42 +0100

    Fixed website redirects
    Since we use our own versioning scheme (currently on 0.1.0), LL's
    web stuff chokes on not knowing the version. Resolve by forcing
    the version to 2.1.0, as well as forcing the channel to
    "Second Life Release".

--- website/app/releasenotes.php
+++ website/app/releasenotes.php
@@ -3,4 +3,4 @@
 $version = explode('.', $_GET['version']);
 array_pop($version);
 $version_short = implode('.', $version);
-header("Location: http://wiki.secondlife.com/wiki/Release_Notes/Second_Life_Release/{$version_short}");
+header("Location: http://wiki.secondlife.com/wiki/Release_Notes/Second_Life_Release/2.1.0");
--- website/help/help.php
+++ website/help/help.php
@@ -7,7 +7,7 @@ $topic = str_replace('/', '', $_GET['topic']);
 header("HTTP/1.1 301 Moved Permanently");
 if(!file_exists("{$topic}.html"))
 {
-    header("Location: http://viewer-help.secondlife.com/{$language}/Second%20Life%20Release/{$version}/{$topic}");
+    header("Location: http://viewer-help.secondlife.com/{$language}/Second%20Life%20Release/2.1.0.0/{$topic}");
 }
 else
 {
